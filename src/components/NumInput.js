c217f2ff36b35b550bde8091141b9b873498f375
chef55 <deez5@yandex.ru>
2024-07-05 14:58:12 +0300

    Update NumInput.js

--- src/components/NumInput.js
+++ src/components/NumInput.js
@@ -5,12 +5,10 @@ import { update } from '../slices/inout';
   
   function NumInput(props){
     const dispatch=useDispatch();
-    let current=JSON.parse(JSON.stringify(useSelector((state)=>state.inout.value)))
     return(
       <>
       <input className="input_style" type="number" defaultValue={0} id={props.input_id} onBlur={(event)=>{
-        current[props.input_id]=event.target.value
-        dispatch(update(current))
+        dispatch(update({name:props.input_id,value:event.target.value}))
       }}></input>
       </>
     );
