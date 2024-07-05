d949908634712c039008a498545ee31101bd24c9
chef55 <deez5@yandex.ru>
2024-07-05 11:45:03 +0300

    Add files via upload

---
+++ src/slices/inout.js
@@ -0,0 +1,34 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState={
+    value:{
+    L: 0,
+    Dvn: 0,
+    dst: 0,
+    Kekv: 0,
+    Kto: 0,
+    Kef: 0,
+    Tos: 0,
+    Pvh: 0,
+    Tvh: 0,
+    Q: 0,
+    P: '',
+    T: '',
+  } 
+}
+
+  export const ioSlice=createSlice({
+    name: 'inout',
+    initialState,
+    reducers:{
+      calculate: (state,action)=>{
+        state.value=action.payload;
+        let {L,Dvn,dst,Kekv,Kto,Kef,Tos,Pvh,Tvh,Q,P,T}=state.value
+        state.value.P=parseFloat(L)+parseFloat(Dvn)
+        state.value.T=parseFloat(dst)-parseFloat(Kekv)
+      }
+    }
+  })
+
+  export const { calculate } = ioSlice.actions;
+  export default ioSlice.reducer;
