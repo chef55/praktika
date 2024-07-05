fcaaa210272830ddc5a011a57ae5b918383f3063
chef55 <deez5@yandex.ru>
2024-07-05 11:53:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from "react-redux"
+import {configureStore} from "@reduxjs/toolkit";
+import ioReducer from "./slices/inout.js"
+
+const store=configureStore({
+  reducer:{
+    inout: ioReducer,
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
