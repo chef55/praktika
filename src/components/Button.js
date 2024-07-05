ccb0a7be18932bd51fd6714ca2a1f391071179d1
chef55 <deez5@yandex.ru>
2024-07-05 13:51:13 +0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,13 @@
-import '../App.css';
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { calculate } from '../slices/inout';
-import { currentValues } from './NumInput';
 
 function Button(props){
     const dispatch=useDispatch();
     return(
       <div className="calculate_button center" id="calc_button"
       onClick={()=>{
-        dispatch(calculate({
-          L: currentValues.L,
-          Dvn: currentValues.Dvn,
-          dst: currentValues.dst,
-          Kekv: currentValues.Kekv,
-          Kto: currentValues.Kto,
-          Kef: currentValues.Kef,
-          Tos: currentValues.Tos,
-          Pvh: currentValues.Pvh,
-          Tvh: currentValues.Tvh,
-          Q: currentValues.Q,
-  }))
+        dispatch(calculate())
       }}
       >{props.value}</div>
     )
